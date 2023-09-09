207936d6dedf6f502db2a2cfe89905d6f9a43a84
pawREP <pawREP@users.noreply.github.com>
2023-09-09 06:06:48 +0200

    Formatting

--- libEmblem/src/DateTime.cpp
+++ libEmblem/src/DateTime.cpp
@@ -4,15 +4,17 @@ DateTime DateTime::fromFileTime(FILETIME fileTime) {
     SYSTEMTIME systemTime{};
     FileTimeToSystemTime(&fileTime, &systemTime);
 
+    // clang-format off
     uint64_t cst{};
-    cst = (uint64_t)systemTime.wSecond & 0x3F;
-    cst = (uint64_t)systemTime.wMinute & 0x3F | cst << 0x06;
-    cst = (uint64_t)systemTime.wHour & 0x1F | cst << 0x05;
-    cst = (uint64_t)systemTime.wDay & 0x1F | cst << 0x05;
-    cst = (uint64_t)systemTime.wDayOfWeek & 0x07 | cst << 0x03;
-    cst = (uint64_t)systemTime.wMonth & 0x0F | cst << 0x04;
+    cst = (uint64_t)systemTime.wSecond       & 0x3F;
+    cst = (uint64_t)systemTime.wMinute       & 0x3F  | cst << 0x06;
+    cst = (uint64_t)systemTime.wHour         & 0x1F  | cst << 0x05;
+    cst = (uint64_t)systemTime.wDay          & 0x1F  | cst << 0x05;
+    cst = (uint64_t)systemTime.wDayOfWeek    & 0x07  | cst << 0x03;
+    cst = (uint64_t)systemTime.wMonth        & 0x0F  | cst << 0x04;
     cst = (uint64_t)systemTime.wMilliseconds & 0x3FF | cst << 0x0A;
-    cst = (uint64_t)systemTime.wYear | cst << 0x0C;
+    cst = (uint64_t)systemTime.wYear                 | cst << 0x0C;
+    // clang-format on
 
     return { .fileTime = fileTime, .compressedSystemTime = cst };
 }
