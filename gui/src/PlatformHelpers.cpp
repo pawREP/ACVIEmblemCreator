cffb2248815ecd50d4ae863bf1da943978d1f5a2
pawREP <pawREP@users.noreply.github.com>
2023-09-10 21:44:49 +0200

    Implement bad_hr_exception and throw it in ThrowIfFailed
    This gives us a bit more expressive errors via what() instead of being hit with a generic "bad hr"

--- gui/src/PlatformHelpers.cpp
+++ gui/src/PlatformHelpers.cpp
@@ -1,8 +1,23 @@
 #include "PlatformHelpers.h"
+#include <cassert>
+#include <comdef.h>
 #include <stdexcept>
 
 void ThrowIfFailed(HRESULT hr) {
     if(FAILED(hr)) {
-        throw std::runtime_error("bad hr");
+        throw bad_hr_exception(hr);
     }
+}
+
+bad_hr_exception::bad_hr_exception(HRESULT hr) {
+    char* msgBuf;
+    auto size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                               NULL, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&msgBuf, 0, NULL);
+    assert(size);
+    msg = msgBuf;
+    LocalFree(msgBuf);
+}
+
+char const* bad_hr_exception::what() const {
+    return msg.c_str();
 }
--- gui/src/PlatformHelpers.h
+++ gui/src/PlatformHelpers.h
@@ -1,4 +1,16 @@
 #pragma once
 #include <Windows.h>
+#include <string>
+
+class bad_hr_exception : public std::exception {
+public:
+    bad_hr_exception(HRESULT hr);
+    ~bad_hr_exception() = default;
+
+    [[nodiscard]] char const* what() const override;
+
+private:
+    std::string msg;
+};
 
 void ThrowIfFailed(HRESULT hr);
