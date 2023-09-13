1d48ac817f27560ae83f232b0a6e2f846c36631e
pawREP <pawREP@users.noreply.github.com>
2023-09-13 06:39:01 +0200

    Add AESBlockLen constant

--- libEmblem/src/Crypto.h
+++ libEmblem/src/Crypto.h
@@ -3,6 +3,8 @@
 
 namespace crypto {
 
+    constexpr uint32_t AESBlockLen = 16;
+
     void decryptInplace(uint8_t* data, uint32_t size, const uint8_t* iv, const uint8_t* key);
     void encryptInplace(uint8_t* data, uint32_t size, const uint8_t* iv, const uint8_t* key);
 
