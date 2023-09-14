e2523d5610e1f8c43471e7454347e3ca040d2500
pawREP <pawREP@users.noreply.github.com>
2023-09-14 03:23:20 +0200

    Refactor md5 for better encapsulation

--- libEmblem/src/Checksum.cpp
+++ libEmblem/src/Checksum.cpp
@@ -1,7 +1,7 @@
 #include "Checksum.h"
 #include "md5.h"
 
-std::array<uint8_t, 0x10> md5Checksum(const uint8_t* data, int64_t size) {
+std::array<uint8_t, 0x10> MD5::checksum(const uint8_t* data, int64_t size) {
     MD5Context ctx;
     md5Init(&ctx);
     md5Update(&ctx, const_cast<uint8_t*>(data), size); // This md5 library is sussy..
@@ -11,3 +11,37 @@ std::array<uint8_t, 0x10> md5Checksum(const uint8_t* data, int64_t size) {
     std::copy(std::begin(ctx.digest), std::end(ctx.digest), result.data());
     return result;
 };
+
+class MD5::MD5Impl {
+public:
+    MD5Impl() {
+        md5Init(&ctx);
+    }
+
+    void update(uint8_t* data, int64_t size) {
+        md5Update(&ctx, const_cast<uint8_t*>(data), size);
+    }
+
+    std::array<uint8_t, 0x10> finalize() {
+        md5Finalize(&ctx);
+        std::array<uint8_t, 0x10> result;
+        std::copy(std::begin(ctx.digest), std::end(ctx.digest), result.data());
+        return result;
+    }
+
+private:
+    MD5Context ctx;
+};
+
+MD5::MD5() : impl(std::make_unique<MD5Impl>()) {
+}
+
+MD5::~MD5() = default;
+
+void MD5::update(uint8_t* data, int64_t size) {
+    impl->update(data, size);
+}
+
+std::array<uint8_t, 0x10> MD5::finalize() {
+    return impl->finalize();
+}
--- libEmblem/src/Checksum.h
+++ libEmblem/src/Checksum.h
@@ -1,3 +1,38 @@
+#include "Define.h"
 #include <array>
+#include <memory>
 
-std::array<uint8_t, 0x10> md5Checksum(const uint8_t* data, int64_t size);
+class MD5 {
+    MAKE_NONCOPYABLE(MD5);
+
+public:
+    MD5();
+    MD5(MD5&&) noexcept            = default;
+    MD5& operator=(MD5&&) noexcept = default;
+    ~MD5();
+
+    void update(uint8_t* data, int64_t size);
+    std::array<uint8_t, 0x10> finalize();
+
+    template <typename T>
+    void update(const T& v) {
+        static_assert(std::is_trivially_copyable_v<T>);
+        static_assert(!std::is_pointer_v<T>);
+
+        update((uint8_t*)&v, sizeof(T));
+    }
+
+    template <typename T>
+    void update(T* v, int64_t count) {
+        static_assert(std::is_trivially_copyable_v<T>);
+        static_assert(!std::is_pointer_v<T>);
+
+        update((uint8_t*)v, count * sizeof(T));
+    }
+
+    static std::array<uint8_t, 0x10> checksum(const uint8_t* data, int64_t size);
+
+private:
+    class MD5Impl;
+    std::unique_ptr<MD5Impl> impl;
+};
